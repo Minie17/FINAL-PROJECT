babe473520f11d3fc28e1315dafad77f7b5fa255
Minie17 <chaitalitagade17@gmail.com>
2021-09-01 21:33:22 +0530

    factorial using recursive method

---
+++ Inheritance demo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Inheritance_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { }
+       
+            public class User
+            { 
+            public string  firstName;
+            private string Identity;
+            public User()
+            {
+                Console.WriteLine("Base Class Constructor");
+            }
+            public void GetUserInfo(string ID)
+            {
+                Identity = ID;
+                Console.WriteLine("firstName: {0}", firstName);
+                Console.WriteLine("Identity: {0}", Identity);
+            }
+        }
+        public class Details : User
+        {
+            public int Age;
+            public Details()
+            {
+                Console.WriteLine("Child Class Constructor");
+            }
+            public void GetAge()
+            {
+                Console.WriteLine("Age: {0}", Age);
+            }
+        }
+        class student
+        {
+            static void Main(string[] args)
+            {
+                Details d = new Details();
+                d.firstName = "Chaitrali";
+                d.Age= 23;
+                d.GetUserInfo("Hyderabad");
+                d.GetAge();
+                Console.WriteLine("\nPress Any Key to Exit..");
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+   
--- factorial/Program.cs
+++ factorial/Program.cs
@@ -2,11 +2,30 @@
 
 namespace factorial
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			static int getFactorial(int number)
+			{
+				if (number == 1)
+				{
+					return 1;
+				}
+				return number * getFactorial(number - 1);
+			}
+
+			
+			{
+				int givenNumber;
+
+				Console.WriteLine("Enter a number to find the factorial ");
+				givenNumber = Convert.ToInt32(Console.ReadLine());
+
+				Console.WriteLine("Factorial : {0}", getFactorial(givenNumber));
+
+			}
+		}
+	}
 }
+		
