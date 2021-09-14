87cf103fa2674f5e66e8b6adb54bcae3e655f4c0
chaitrali_ tagde <chaitalitagade17@gmail.com>
2021-09-14 16:28:05 +0530

    project

---
+++ Restaurant system  chaitrali/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Restaurant_system__chaitrali
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
